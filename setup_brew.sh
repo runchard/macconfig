0abad41d303b660686d4be820d221f9d56f6ff42
Runchard <50762096+runchard@users.noreply.github.com>
2020-11-17 21:38:49 +0800

    fix caskroom -> homebrew

--- setup_brew.sh
+++ setup_brew.sh
@@ -5,8 +5,9 @@ set -ex
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
+brew tap homebrew/cask
+brew update
 brew install git leiningen go z packer awscli tig fzf gh trash starship kitty diff-so-fancy corretto
+# rust toolchain
 brew install exa procs dust bat ripgrep fd
-brew tap caskroom/cask
-brew tap caskroom/versions
 brew cask install karabiner-elements dash sequel-pro emacs caffeine docker hammerspoon alfred telegram the-unarchiver gpg-suite
